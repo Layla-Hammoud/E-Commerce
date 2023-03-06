b0afb74b31a6e709dfb34409ea5294ab05d2ff21
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-06 23:43:45 +0200

    fetching signin api from backend

--- js/register.js
+++ js/register.js
@@ -15,3 +15,29 @@ document.getElementById("have_account").onclick = () => {
   pages[1].style.display = "flex";
   pages[2].style.display = "none";
 };
+
+const signin_btn = document.getElementById("signin_btn");
+
+console.log(signin_btn);
+
+signin_btn.addEventListener("click", async () => {
+  console.log("clicked");
+  const sign_email_input = document.getElementById("sign_email_input");
+  const sign_password_input = document.getElementById("sign_password_input");
+
+  const requestBody = new FormData();
+  requestBody.append("credentials", sign_email_input.value);
+  requestBody.append("password", sign_password_input.value);
+
+  const response = await axios({
+    method: "post",
+    url: "http://localhost/ecommerce-backend/signin.php",
+    data: requestBody,
+  });
+  console.log(response.data);
+
+  if (response.data.id) {
+    window.location.href = "../index.html";
+    localStorage.setItem("user_id", response.data.id);
+  }
+});
