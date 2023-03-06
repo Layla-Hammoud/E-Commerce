aa89031fced09114e13da6cebec7f942d0249ebb
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-06 16:17:11 +0200

    Getting DOM from HTML

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const display_full_name = document.getElementById("display_full_name");
+const all_categories = document.getElementById("all_categories");
+const all_products = document.getElementById("all_products");
+const baseURL = "http://localhost";
+
+request_body.append("user_id", 1);
