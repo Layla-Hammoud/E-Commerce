71b3e875558d956da2514ca40bba70644652c5dc
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-06 23:46:04 +0200

    adding add_product fnc

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,8 @@
+let submit_btn = document.getElementById("submit_btn");
+submit_btn.addEventListener("click", add_product);
 
-
-
-
+function add_product() {
+  let name = document.getElementById("fname").value;
+  let brand = document.getElementById("brand").value;
+  let price = document.getElementById("price").value;
+}
