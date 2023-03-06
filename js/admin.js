9882cede5576072a2ebd819bd6fac1a30f14467a
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-06 23:49:28 +0200

    creating newform data obj

--- js/admin.js
+++ js/admin.js
@@ -5,4 +5,9 @@ function add_product() {
   let name = document.getElementById("fname").value;
   let brand = document.getElementById("brand").value;
   let price = document.getElementById("price").value;
+
+  let data = new FormData();
+  data.append("name", name);
+  data.append("brand", brand);
+  data.append("price", price);
 }
