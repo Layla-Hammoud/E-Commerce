447851663a9a7afb2662b00fec454b99c4ce801f
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-06 16:18:50 +0200

    fetching user full name from backend

--- js/index.js
+++ js/index.js
@@ -3,4 +3,15 @@ const all_categories = document.getElementById("all_categories");
 const all_products = document.getElementById("all_products");
 const baseURL = "http://localhost";
 
+const request_body = new FormData();
+
 request_body.append("user_id", 1);
+
+axios({
+  method: "post",
+  url: `${baseURL}/ecommerce-backend/signin.php`,
+  data: request_body,
+}).then((res) => {
+  const user = res.data.user;
+  display_full_name.innerText = `${user.first_name} ${user.last_name}`;
+});
