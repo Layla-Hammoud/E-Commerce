b12a4d2cf607617c8b6d07a96366abaece6f2385
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-06 22:33:31 +0200

    moving btw pages

--- js/register.js
+++ js/register.js
@@ -10,3 +10,8 @@ document.getElementById("SignIn").onclick = () => {
   pages[1].style.display = "none";
   pages[2].style.display = "none";
 };
+document.getElementById("have_account").onclick = () => {
+  pages[0].style.display = "none";
+  pages[1].style.display = "flex";
+  pages[2].style.display = "none";
+};
